42c0c31204cca44fe2b2cf18483dcac24432079f
Joel Weber <joel.weber@jkweb.ch>
2023-05-09 09:54:12 +1000

    Gitignore and colors

---
+++ OzFair/Model/Colors.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  OzFair
+//
+//  Created by Joel Weber on 09.05.23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let heading = Color(red: 22/255, green: 23/255, blue: 22/255)
+    static let text = Color(red: 22/255, green: 23/255, blue: 22/255)
+    static let subheading = Color(red: 30/255, green: 108/255, blue: 47/255)
+    static let navActive = Color(red: 30/255, green: 108/255, blue: 47/255)
+    static let navInactive = Color(red: 120/255, green: 171/255, blue: 131/255)
+    static let frameBG = Color(red: 250/255, green: 250/255, blue: 250/255)
+    static let negativeAmount = Color(red: 189/255, green: 60/255, blue: 20/255)
+    static let positiveAmount = Color(red: 83/255, green: 176/255, blue: 82/255)
+    static let fadedText = Color(red: 161/255, green: 161/255, blue: 161/255)
+    static let groupCardColor = Color(red: 239/255, green: 255/255, blue: 243/255)
+    static let overlayCard = Color(red: 239/255, green: 245/255, blue: 254/255)
+    static let cardColor1 = Color(red: 191/255, green: 91/255, blue: 145/255)
+    static let cardColor2 = Color(red: 0/255, green: 126/255, blue: 167/255)
+}
