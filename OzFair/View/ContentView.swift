aaa699b4520993ab7b9dc9266c4165c68597b072
MagnusMga <magnusmga@gmail.com>
2023-05-14 18:46:18 +1000

    adding ScrollView

--- OzFair/View/ContentView.swift
+++ OzFair/View/ContentView.swift
@@ -42,9 +42,12 @@ struct ContentView: View {
     }
 }
 
+
 struct HomeView: View {
     //Transactions list
     let transactions = [Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00),
+                        Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00),
+                        Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00),
                         Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00),
                         Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00),
                         Transaction(title: "To Magnus", date: "08 May 2023", amount: 59.00)]
@@ -53,69 +56,71 @@ struct HomeView: View {
     var body: some View {
         
         // Whole stack
-        VStack(alignment: .leading) {
-            // Title
-            VStack(alignment:.leading) {
-                Text("Accounts and Cards")
-                .fontWeight(.semibold)
-                .font(.caption)
-                .foregroundColor(.subheading)
-                .lineSpacing(20)
-                Text("Welcome Fabian!")
-                .fontWeight(.semibold)
-                .font(.largeTitle)
-                .foregroundColor(.heading)
-                .lineSpacing(38)
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack(alignment: .leading) {
+                // Title
+                VStack(alignment:.leading) {
+                    Text("Accounts and Cards")
+                        .fontWeight(.semibold)
+                        .font(.caption)
+                        .foregroundColor(.subheading)
+                        .lineSpacing(20)
+                    Text("Welcome Fabian!")
+                        .fontWeight(.semibold)
+                        .font(.largeTitle)
+                        .foregroundColor(.heading)
+                        .lineSpacing(38)
 
-            }
-            Spacer()
-            // Tabs
-            Tabs(items: [("Accounts", 58, true), ("Cards", 38, false)])
-            Spacer()
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            BalanceCard(title: "Spending 1", amount: 2000.05, bgColor: .cardColor1)
-                            BalanceCard(title: "Spending 2", amount: 100.23, bgColor: .cardColor2)
-                            BalanceCard(title: "Spending 3", amount: 24.50, bgColor: .cardColor2)
-                            BalanceCard(title: "Spending 4", amount: 1500.45, bgColor: .cardColor2)
-                        }
-                        .padding(20)
-                    }
-            .padding(-30)
-            
-            Spacer()
-            // Recent Transactions
-            Text("Recent Transactions")
-            .fontWeight(.semibold)
-            .font(.headline)
-            .foregroundColor(.text)
-            .lineSpacing(20)
-            .padding(.bottom, 20)
-            
-            Tabs(items: [
-                ("Today", 38, true),
-                ("This Week", 2, false),
-                ("This Month", 2, false),
-                ("6 Months", 2, false),
-            ])
-            .padding(.bottom, 10)
-            
-            VStack {
-                ForEach(transactions) { transaction in
-                    ListItem(title: transaction.title, date: transaction.date, amount: transaction.amount)
                 }
-             //Spacer(minLength: 10)// set a minimum length for the Spacer
+                Spacer()
+                // Tabs
+                Tabs(items: [("Accounts", 58, true), ("Cards", 38, false)])
+                Spacer()
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        BalanceCard(title: "Spending 1", amount: 2000.05, bgColor: .cardColor1)
+                        BalanceCard(title: "Spending 2", amount: 100.23, bgColor: .cardColor2)
+                        BalanceCard(title: "Spending 3", amount: 24.50, bgColor: .cardColor2)
+                        BalanceCard(title: "Spending 4", amount: 1500.45, bgColor: .cardColor2)
+                    }
+                    .padding(20)
+                }
+                .padding(-30)
+
+                Spacer()
+                // Recent Transactions
+                Text("Recent Transactions")
+                    .fontWeight(.semibold)
+                    .font(.headline)
+                    .foregroundColor(.text)
+                    .lineSpacing(20)
+                    .padding(.bottom, 20)
+
+                Tabs(items: [
+                    ("Today", 38, true),
+                    ("This Week", 2, false),
+                    ("This Month", 2, false),
+                    ("6 Months", 2, false),
+                ])
+                .padding(.bottom, 10)
+
+                VStack {
+                    ForEach(transactions) { transaction in
+                        ListItem(title: transaction.title, date: transaction.date, amount: transaction.amount)
+                    }
+                    //Spacer(minLength: 10)
+                }
             }
+            .frame(maxWidth: .infinity, alignment: .top)
+            .padding(.vertical, 15)
+            .padding(.horizontal, 30)
         }
-        .frame(maxWidth: .infinity, alignment: .top)
-        .padding(.vertical, 15)
-        .padding(.horizontal, 30)
-        .background(Color.frameBG)
         // Whole Stack End
-        
+        .background(Color.frameBG)
     }
 }
+
 
 //Transactions
 struct Transaction: Identifiable {
