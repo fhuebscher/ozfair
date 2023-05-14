df7db40c228603778dd4699e43e069b36d18423f
MagnusMga <magnusmga@gmail.com>
2023-05-15 09:44:28 +1000

    transfer bg full height

--- OzFair/ViewController/ContentView.swift
+++ OzFair/ViewController/ContentView.swift
@@ -206,7 +206,7 @@ struct TransferView: View {
                                     }, secondaryButton: .cancel())
                             }
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .frame(maxHeight: .infinity, alignment: .leading)
         .padding(.vertical, 15)
         .padding(.horizontal, 30)
         .background(Color.frameBG)
