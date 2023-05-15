ebc9418fa04d20df242c645b3bc3d94beb074db9
Fabian Roth <84498951+Faro-Enuff@users.noreply.github.com>
2023-05-15 20:31:00 +1000

    Update ConverterPresenter.swift

--- OzFair/Model/ConverterPresenter.swift
+++ OzFair/Model/ConverterPresenter.swift
@@ -22,18 +22,22 @@ struct RatesDisplayable {
     let exchange: ExchangeMoney
     
     func isActive() -> Bool {
+        // Active rates display ? Check on exchange source, currency, and active status ->
         return source == exchange.source && self.currency == exchange.currency && exchange.active
     }
     
     func canExchange() -> Bool {
+        // Can exchange be performed? Check on available money > exchange value + rate
         return self.money >= self.exchange.value * self.exchange.rate
     }
     
     func exchangeEnabled() -> Bool {
+        // Check for exchange being enabled or not
         return self.exchange.value > 0 && self.canExchange()
     }
     
     func getDisplayableExchangeValue() -> String {
+        // Exchange value (based on exchange rate+value) -> return and display
         let value = self.exchange.value * self.rate
         return String(format: "%.2f", value)
     }
