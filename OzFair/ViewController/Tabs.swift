7e92308c1185840f1ffe809bc72da86ab6860e68
Joel Weber <joel.weber@jkweb.ch>
2023-05-15 15:42:16 +1000

    Tabs fix

--- OzFair/ViewController/Tabs.swift
+++ OzFair/ViewController/Tabs.swift
@@ -12,17 +12,17 @@ struct Tabs: View {
     let items: [(String, CGFloat, Bool)]
     var body: some View {
         HStack(spacing: 25) {
-            ForEach(items.indices) { index in
-                let color = items[index].2 ? Color.navActive : Color.text
-                let opacity: Double = items[index].2 ? 1 : 0
+            ForEach(items, id: \.0) { item in
+                let color = item.2 ? Color.navActive : Color.text
+                let opacity: Double = item.2 ? 1 : 0
                 VStack {
-                    Text(items[index].0)
+                    Text(item.0)
                         .fontWeight(.semibold)
                         .font(.caption)
                         .lineSpacing(20)
                         .foregroundColor(color)
                     RoundedRectangle(cornerRadius: 2)
-                        .frame(width: items[index].1, height: 2)
+                        .frame(width: item.1, height: 2)
                         .foregroundColor(color)
                         .opacity(opacity)
                 }
